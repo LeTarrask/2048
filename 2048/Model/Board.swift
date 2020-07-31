66693297c215d2394816e53d962d002e3c5dab72
Alex Luna <14889546+LeTarrask@users.noreply.github.com>
2020-07-31 12:45:05 +0100

    Removed testing board code

--- 2048/Model/Board.swift
+++ 2048/Model/Board.swift
@@ -23,16 +23,5 @@ struct Board {
         for _ in 1...size { grid.append(line) }
 
         grid[Int.random(in: 0..<size)][Int.random(in: 0..<size)].value = 2
-
-        // MARK: - Testing board
-//        for lines in [[0,2,2,0], [2,2,2,8], [256,256,2,4], [2,0,2,0]] {
-//            var line = [Tile]()
-//            for value in lines {
-//                var tile = Tile()
-//                tile.value = value
-//                line.append(tile)
-//            }
-//            grid.append(line)
-//        }
     }
 }
