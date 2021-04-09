f747b7359e2864c2d570f239b2469a79b80a3ee3
leTarrask <14889546+LeTarrask@users.noreply.github.com>
2021-04-09 17:31:04 +0100

    Run tests successfully

--- 2048Tests/_048Tests.swift
+++ 2048Tests/_048Tests.swift
@@ -10,19 +10,6 @@ import XCTest
 // swiftlint:disable type_name
 class _048Tests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
     func testBoardCreation() throws {
         let engine = GameEngine()
         let allTileValues = engine.board.grid.flatMap { $0 }.map { $0.value }
@@ -46,12 +33,4 @@ class _048Tests: XCTestCase {
         XCTAssertEqual(engine.transformArray(array: [256, 256, 2, 4]), [512, 2, 4, 0])
         XCTAssertEqual(engine.transformArray(array: [2, 0, 2, 0]), [4, 0, 0, 0])
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
